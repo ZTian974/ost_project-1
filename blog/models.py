e0e03056d79731568ae4d88eba73fac19753f8d6
wenjiechen <cwjcourage@gmail.com>
2013-12-15 01:53:03 -0500

    database design

--- blog/models.py
+++ blog/models.py
@@ -5,22 +5,61 @@ import re
 # Create your models here.
 
 def validate_letter(value):
+    # can only have digital, letter and '_'
     p = re.compile(r'^[\d\w_]{1,30}$')
     if p.match(value) == None:
         raise ValidationError("The number of letters of Username must less than 30 and letters can only have digital, letter and '_'.")
 
 class User(models.Model):
-    #can only have digital, letter and '_' 
-    name = models.CharField(max_length=30,db_index=True,unique=True,validators=[validate_letter])
+    username = models.CharField(max_length=30,db_index=True,unique=True, validators=[validate_letter])
     password = models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return '%s' %self.name
+        
+     def get_absolute_url(self):
+        pass
+#        return reverse('blog:view_blog_category', kwargs={'username':self.username})
 
 class Blog(models.Model):
-    # a user can create many blogs
     user = models.ForeignKey(User)
+    title = models.CharField(max_length=150,db_index=True)
+    slug = models.CharField(max_length=150,db_index=True, validators=[validate_letter])
+    creation_date = models.DateTimeField(auto_now_add=True)
     
+    def __unicode__(self):
+        return '%s' %self.title
 
+    def get_absolute_url(self):
+        if slug.strip() == '':
+            slug = title.replace()
+        pass
+    
+    class Meta:
+        #default ordering, “-” indicates descending order, without “-” will be ordered ascending
+        ordering = ('-creation_date',)
 
 class Post(models.Model):
-    
     blog = models.ForeignKey(Blog)
+    tags = models.ManyToManyField('Tag',blank=True)
+    title = models.CharField(max_length=150,db_index=True)
+    body = models.TextField()
+    slug = models.CharField(max_length=150,db_index=True, validators=[validate_letter])
+    creation_date = models.DateTimeField(auto_now_add=True)    
+    modification_date = models.DateTimeField(auto_now=True)    
+    
+    def __unicode__(self):
+        return '%s' %self.title
+
+    def get_absolute_url(self):
+        pass
+
+    class Meta:
+        ordering = ('-modification_date',)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50,db_index=True)
+        
+    class Meta:
+        ordering = ('title',)
+
