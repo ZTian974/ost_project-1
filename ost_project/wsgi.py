c0d01952e991ca501b9652185f265fac8bb1f990
wenjiechen <cwjcourage@gmail.com>
2013-12-16 22:56:06 -0500

    just save

--- ost_project/wsgi.py
+++ ost_project/wsgi.py
@@ -10,5 +10,6 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ost_project.settings")
 
+sys.path.append('/home/ubuntu/ost_project/')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
